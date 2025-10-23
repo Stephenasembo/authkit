651d74efb7f61445d63d3f7c2e5bf3fc8afd0fc4
Stephenasembo <165886904+Stephenasembo@users.noreply.github.com>
2025-10-23 22:22:54 +0300

    Feat: Add A Login Form

--- src/components/AuthContainer.jsx
+++ src/components/AuthContainer.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import GoogleButton from "./GoogleButton";
 import SignUpForm from "./SignupForm";
+import LoginForm from "./LoginForm"
 
 export default function AuthContainer() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return(
     <div
     className="border w-full max-w-xs mx-auto p-6 bg-white font-roboto rounded-xl shadow-md flex flex-col gap-5
@@ -20,7 +24,43 @@ export default function AuthContainer() {
       <p
       className="text-sm text-[#5f6368] dark:text-[#9aa0a6]"
       >Fields marked with an asterisk(*) are mandatory.</p>
-      <SignUpForm />
+      {isLogin ?
+      <div>
+        <LoginForm />
+        <p
+        className="mt-2"
+        >
+          <span>
+            Don't have an account?
+          </span> {" "}
+          <button
+          type="button"
+          onClick={() => setIsLogin(false)}
+          className="text-blue-600 dark:text-blue-400 font-medium hover:underline cursor-pointer
+          hover:text-blue-800 dark:hover:text-blue-600">
+            Sign up
+          </button>
+        </p>
+
+      </div> :
+      <div>
+        <SignUpForm />
+        <p
+        className="mt-2"
+        >
+          <span>
+            Already have an account?
+          </span> {" "}
+          <button
+          type="button"
+          onClick={() => setIsLogin(true)}
+          className="text-blue-600 dark:text-blue-400 font-medium hover:underline cursor-pointer
+          hover:text-blue-800 dark:hover:text-blue-600">
+            Login
+          </button>
+        </p>
+      </div>
+      }
     </div>
   )
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+import { useFormik } from "formik"
+import { UserIcon, EyeIcon } from "./svgs"
+import * as Yup from "yup"
+
+export default function LoginForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .trim()
+        .min(3, "Username must be at least 3 characters")
+        .required("Username is required"),
+      password: Yup.string()
+        .trim()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+    })
+  })
+
+  function viewPassword(e) {
+    e.preventDefault()
+    setPasswordVisible(!passwordVisible)
+  }
+  
+  return (
+    <form
+    className="flex flex-col gap-4 mt-2"
+    onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:*</label>
+        <span
+          className={`input-container ${formik.touched.username && formik.errors.username ? "error" : ""}`}
+          >
+          <UserIcon />
+          <input
+          className="input"
+          id="username"
+          name="username"
+          type="username"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          placeholder="Enter your username"
+          onBlur={formik.handleBlur}
+          />
+        </span>
+        {formik.touched.username && formik.errors.username ? (
+          <p className="text-red-500 text-sm mt-1">{formik.errors.username}</p>
+        ) : null}
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:*</label>
+        <span
+        className={`input-container ${formik.touched.password && formik.errors.password ? "error" : ""}`}
+        >
+          <input
+          className={`input ${formik.touched.password && formik.errors.password ? "border-red-500" : ""}`}
+          id="password"
+          name="password"
+          type={passwordVisible? "text" : "password"}
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder="Enter your password"
+          onBlur={formik.handleBlur}
+          />
+          <EyeIcon
+          onClick={viewPassword}
+          />
+        </span>
+        {formik.touched.password && formik.errors.password ? (
+          <p className="text-red-500 text-sm mt-1">{formik.errors.password}</p>
+        ) : null}
+      </div>
+
+      <button
+      className="w-full border rounded-xl h-12 flex cursor-pointer items-center justify-center
+      font-roboto text-[#1f1f1f] bg-neutral-300 font-medium hover:bg-[#303030]/8
+      transition-all duration-200 border-[#747775]
+      hover:shadow-[0_1px_2px_0_rgba(60,64,67,0.3),0_1px_3px_1px_rgba(60,64,67,0.15)]
+      dark:border-[#747775] dark:bg-neutral-800 dark:text-[#e3e3e3] dark:hover:bg-[#2a2a2a]"
+      type="submit"
+    >Submit</button>
+    </form>
+  )
+}
